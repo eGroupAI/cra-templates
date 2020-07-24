e8ad49772f8e177b53b388c76bba63d40a6ad485
SP_Penguin <abrcdf1023@gmail.com>
2020-07-24 16:41:10 +0800

    update fetcher timeout

--- packages/cra-template-typescript/template/src/redux/apis.js
+++ packages/cra-template-typescript/template/src/redux/apis.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const fetcher = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL,
-  timeout: 3000,
+  timeout: 1000 * 60 * 10,
   headers: {
     'Content-Type': 'application/json;charset=UTF-8'
   }
