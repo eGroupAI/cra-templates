b9d786ac2070c936f276d97e444ed9c346314732
SP_Penguin <abrcdf1023@gmail.com>
2020-09-16 10:25:01 +0800

    [chore] upgrade to typescript v4 1. migrate to airbnb eslint config 2. update dependencies

---
+++ packages/cra-template-typescript/template/.eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  plugins: [
+    "@typescript-eslint",
+    "jest"
+  ],
+  env: {
+    browser: true
+  },
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "airbnb-typescript",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jest/recommended",
+    "prettier",
+    "prettier/react",
+    "prettier/@typescript-eslint",
+  ],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {}
+    },
+  },
+  rules: {
+    "react/prop-types": "off",
+    "react/jsx-props-no-spreading": "off",
+    "react/destructuring-assignment": "off",
+    "import/prefer-default-export": "off",
+    "react/button-has-type": "off",
+    "jsx-a11y/media-has-caption": "off",
+    "react/jsx-no-duplicate-props": ["error", { ignoreCase: false }],
+    "no-plusplus": "off",
+    "radix": "off",
+    "@typescript-eslint/no-shadow": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    // TODO: We should reopen this rule after all component refactor to ts.
+    "@typescript-eslint/ban-ts-comment": "off",
+  }
+};
